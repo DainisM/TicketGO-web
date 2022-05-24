c6fe6e31c4cb256173e11d0df0feed2f28a93ab9
DainisM <moisejenko11@gmail.com>
2022-05-24 11:22:10 +0200

    Added user logout and div changes depending on if user is logged or not

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
+
+import { useRealmApp } from "../../RealmApp";
+
 import "./styles.scss";
 import Logo from "../../assets/TicketGO_Logo.png";
 
 function Header() {
+	//Accessing Realm App
+	const app = useRealmApp();
+
+	//Method to handle user signout
+	const handleSignOut = async () => {
+		//Logout current user
+		await app.currentUser.logOut();
+		//Refresh page
+		window.location.reload();
+	};
+
 	return (
 		<header className="header">
 			<div className="wrap">
@@ -18,17 +34,36 @@ function Header() {
 					<input placeholder="Search" />
 				</div>
 
-				<div className="actions">
-					<ul>
-						<li key="1">
-							<Link to="/register">Register</Link>
-						</li>
+				{app.currentUser === null ||
+				app.currentUser.providerType === "anon-user" ? (
+					<div className="actions">
+						<ul>
+							<li key="1">
+								<Link to="/register">Register</Link>
+							</li>
 
-						<li key="2">
-							<Link to="/login">Login</Link>
-						</li>
-					</ul>
-				</div>
+							<li key="2">
+								<Link to="/login">Login</Link>
+							</li>
+						</ul>
+					</div>
+				) : (
+					<div className="actions">
+						<ul className="loggedUl">
+							<li>
+								<Link to="/profile">
+									{" "}
+									<FontAwesomeIcon icon={faUserCircle} size="xl" /> My Account
+								</Link>
+							</li>
+							<li>
+								<button className="signOutBtn" onClick={handleSignOut}>
+									Sign out
+								</button>
+							</li>
+						</ul>
+					</div>
+				)}
 			</div>
 			<div className="navBar">
 				<ul>
